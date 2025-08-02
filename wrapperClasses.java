e2e6d93e17d13f8b2c467a0d6ced9bca61743ef7
pranay-svg <padakantipranaykumar@gmail.com>
2025-08-02 09:27:26 +0530

    Add files via upload

---
+++ wrapperClasses.java
@@ -0,0 +1,16 @@
+package MyProject_Day1;
+
+public class wrapperClasses {
+	public static void main(String args[]) {
+		
+		//AutoBoxing
+		int a=10;
+		Integer obj=a;
+		System.out.println("Autoboxing " +obj);
+		
+		//UnBoxing
+		Integer ob1=20;
+		int b=ob1;
+		System.out.println("Unboxing " +b);
+	}
+}
