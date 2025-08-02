64c4dbfbd5688c466819de47fb2ae8c55525d45f
pranay-svg <padakantipranaykumar@gmail.com>
2025-08-02 09:28:46 +0530

    Add files via upload

---
+++ methodExample.java
@@ -0,0 +1,28 @@
+package MyProject_Day1;
+
+public class methodExample {
+	public int display() {
+		int a=10;
+		return a;
+	}
+	public static void main(String args[]) {
+		methodExample ob=new methodExample();
+		int b=ob.display();
+		System.out.println(b);
+	}
+}
+
+
+/* package MyProject_Day1;
+
+public class methodExample {
+	public int display(10) {
+		
+		return a;
+	}
+	public static void main(String args[]) {
+		methodExample ob=new methodExample();
+		int b=ob.display(10);
+		System.out.println(b);
+	}
+}		*/
